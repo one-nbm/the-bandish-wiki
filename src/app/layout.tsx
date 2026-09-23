ec9986c23a67e541573a524df1cd1047ef96efc1
Neil Lote <loteneil2011@gmail.com>
2026-09-22 17:47:25 -0700

    beta-1.5.4-index

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Google_Sans_Flex } from "next/font/google";
 import { ThemeProvider } from "./ThemeProvider";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GoogleAnalytics } from '@next/third-parties/google';
 import AuthButton from "@/components/AuthButton";
 import Navbar from "@/components/Navbar";
 import "./globals.css";
@@ -49,6 +50,7 @@ export default function RootLayout({
           {children}
           <SpeedInsights />
         </ThemeProvider>
+        <GoogleAnalytics gaId="G-XELFB1P2TX" />
       </body>
     </html>
   );
